eea995820a290e47a7576664f08633bef9a9ffe4
Paul Muston <paul.muston@gmail.com>
2026-07-20 20:04:52 +0300

    graphdb 0.15.0

--- Formula/graphdb.rb
+++ Formula/graphdb.rb
@@ -1,28 +1,28 @@
 class Graphdb < Formula
   desc "In-memory graph database with a Cypher subset over HTTP/JSON"
   homepage "https://github.com/pmuston/homebrew-graphdb"
-  version "0.14.0"
+  version "0.15.0"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.14.0/graphdb-v0.14.0-darwin-arm64.tar.gz"
-      sha256 "85b51ce95fbbf2a0f1d81d6add2408cf64c5f03af377e8f37ba0d5c4f910e90f"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.15.0/graphdb-v0.15.0-darwin-arm64.tar.gz"
+      sha256 "2c518d7a682464864454fbd745fc11e2f099a6522f4010a17f3501bd53190ca2"
     end
     on_intel do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.14.0/graphdb-v0.14.0-darwin-amd64.tar.gz"
-      sha256 "892070a60c5ae28ddf2666cbe67da23f4afde4e7cd5d201f3bbd55510dca9ca5"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.15.0/graphdb-v0.15.0-darwin-amd64.tar.gz"
+      sha256 "1eb4d6d97dea02720420ba71a8c7f400a1a1870804a989e23ad513797894c78f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.14.0/graphdb-v0.14.0-linux-arm64.tar.gz"
-      sha256 "37e62788fbbb00a687d59097b24bf48198e1d0e1612a31e391aa684dce7866d1"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.15.0/graphdb-v0.15.0-linux-arm64.tar.gz"
+      sha256 "2c92b0abcfceeb1028f21be13b9586eef3b7da0b79c7ec0c58c5ded37c8927b0"
     end
     on_intel do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.14.0/graphdb-v0.14.0-linux-amd64.tar.gz"
-      sha256 "d670f9ac259378ae5654b291c5379b33d0b3795a7704064c615c219870676ca0"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.15.0/graphdb-v0.15.0-linux-amd64.tar.gz"
+      sha256 "81a04655b40573e7d59e1a396952878167f87b6fa1ce2e5d51b9e48377ff8a36"
     end
   end
 
