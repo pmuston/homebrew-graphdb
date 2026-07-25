f7e1522fade573953ed8fd8ba89ad175ee4d9284
Paul Muston <paul.muston@gmail.com>
2026-07-25 07:20:23 +0300

    graphdb 0.20.0

--- Formula/graphdb.rb
+++ Formula/graphdb.rb
@@ -1,28 +1,28 @@
 class Graphdb < Formula
   desc "In-memory graph database with a Cypher subset over HTTP/JSON"
   homepage "https://github.com/pmuston/homebrew-graphdb"
-  version "0.19.0"
+  version "0.20.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.19.0/graphdb-v0.19.0-darwin-arm64.tar.gz"
-      sha256 "f1629938649f36aaab6a36e45687b7c6c0fb9a045b8de961f41fd450d9fc7f84"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.20.0/graphdb-v0.20.0-darwin-arm64.tar.gz"
+      sha256 "0f244e321254db5d566e044d9aa351d039124e0ac9d355075cd13bf4bf99e1d8"
     end
     on_intel do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.19.0/graphdb-v0.19.0-darwin-amd64.tar.gz"
-      sha256 "4ba4296ea7bf5436854f43adeaa38b88c6d2cfee94467ee3fd43959ed68bc5a4"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.20.0/graphdb-v0.20.0-darwin-amd64.tar.gz"
+      sha256 "03426976c839251f7e222db22c4862bcd29c805b88320927df8ce3cdf54cceb3"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.19.0/graphdb-v0.19.0-linux-arm64.tar.gz"
-      sha256 "45b7bdb7015ed50e9542d03d806544ad4a1ea790844f354db98ef94167b91fd8"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.20.0/graphdb-v0.20.0-linux-arm64.tar.gz"
+      sha256 "988697eb1fb19b8029aefe1ae72b75fdd4c3ff98b87054a1344abef985c4eacb"
     end
     on_intel do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.19.0/graphdb-v0.19.0-linux-amd64.tar.gz"
-      sha256 "014a7551dfc40a3199d12fbb3cdc84cb25b0c8113270a72b7783545cf4196f14"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.20.0/graphdb-v0.20.0-linux-amd64.tar.gz"
+      sha256 "30eeafca257535d3ecb7e0347fe578eafa022535eec2312dd040b2891d54b6e0"
     end
   end
 
