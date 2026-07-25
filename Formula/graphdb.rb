e0a18afe5526e2a3f62f31b0edcc5e970e990ebc
Paul Muston <paul.muston@gmail.com>
2026-07-25 09:01:34 +0300

    graphdb 0.23.0

--- Formula/graphdb.rb
+++ Formula/graphdb.rb
@@ -1,33 +1,34 @@
 class Graphdb < Formula
   desc "In-memory graph database with a Cypher subset over HTTP/JSON"
   homepage "https://github.com/pmuston/homebrew-graphdb"
-  version "0.22.0"
+  version "0.23.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.22.0/graphdb-v0.22.0-darwin-arm64.tar.gz"
-      sha256 "2d1c9df12b7106a5ce3d1bc2616aa9415e5f6cc0c03f79cb56cd9b3a0182f650"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.23.0/graphdb-v0.23.0-darwin-arm64.tar.gz"
+      sha256 "cc75b2025d509382be11d9498849106ee9855d8ea6de70bffde70b043589fad5"
     end
     on_intel do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.22.0/graphdb-v0.22.0-darwin-amd64.tar.gz"
-      sha256 "1d022d6f51c48194eca4281f29d09c9ae36eb8c284e6d10b266ae48efcfbc22b"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.23.0/graphdb-v0.23.0-darwin-amd64.tar.gz"
+      sha256 "656506516b104db6728b49e023fb4f767251294b63f37a11d3b499cfd109140d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.22.0/graphdb-v0.22.0-linux-arm64.tar.gz"
-      sha256 "90291bacf1aae24942e888eb9704e98c7b1c3ba961ebff765615c32abf93ff1f"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.23.0/graphdb-v0.23.0-linux-arm64.tar.gz"
+      sha256 "97db4b12f5fc6373e7955f933895789e51d1169a65b0b0b4e9fbbb3bcafe5d85"
     end
     on_intel do
-      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.22.0/graphdb-v0.22.0-linux-amd64.tar.gz"
-      sha256 "9b3dc522c4621295cfcb5cde17306913f59d3de9f4492e0e7a797b116e4b099d"
+      url "https://github.com/pmuston/homebrew-graphdb/releases/download/v0.23.0/graphdb-v0.23.0-linux-amd64.tar.gz"
+      sha256 "026971efcff90624042daee7d518f953235cba99fdecbe6ceca248447730c8fc"
     end
   end
 
   def install
     bin.install "graphdb"
+    man1.install "graphdb.1"
   end
 
   test do
